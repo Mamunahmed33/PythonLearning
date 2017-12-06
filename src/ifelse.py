bd63ba6051607743e2c9d4057c1f48802669d65d
mamunahmed33 <mamunahmed33@gmail.com>
2017-12-06 11:29:28 +0600

    print and if else code is added

---
+++ src/ifelse.py
@@ -0,0 +1,14 @@
+
+x = 25
+
+if x >= 24:
+    print("Y")
+
+name = "Rahim"
+
+if name is "Mamun":
+    print("Mamun Ahmed")
+elif name is "Rahim":
+    print("What's up Rahim")
+else:
+    print("No one here")
