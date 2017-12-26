ff722e6909b3d59411fe1433c993cb6e3e15a2c0
mamunahmed33 <mamunahmed33@gmail.com>
2017-12-26 13:41:56 +0600

    Word Frequency Counter is complete

--- src/FrequwncyCounter.py
+++ src/FrequwncyCounter.py
@@ -14,26 +14,40 @@ def wordList(url):
         content = articleText.string
         if content:
             word = content.lower().split()
-
+           # print(word)
         for eachWord in word:
             if eachWord:
                 wordList.append(eachWord)
                 #print(eachWord)
 
     cleanUpList(wordList)
-    printListEleents(wordList)
+   # printListEleents(wordList)
 
 
 def cleanUpList(wordList):
     cleanWordList = []
-    symbols = "'~!@#$%^&*(){}[]?<>\"/;:-"
+    symbols = "'~!@#$%^&*(){}[]?<>\`\";:-,_."
     for word in wordList:
-        for i in range(0, len(word)):
+        for i in range(0, len(symbols)):
             word = word.replace(symbols[i], "")
         if len(word) > 0:
             cleanWordList.append(word)
 
-    printListEleents(cleanUpList())
+   # printListEleents(cleanUpList())
+    createDictionary(cleanWordList);
+
+def createDictionary(cleanWordList):
+    wordCount = {}
+
+    for word in cleanWordList:
+        if word in wordCount:
+            wordCount[word] += 1
+        else:
+            wordCount[word] = 1
+
+    for key, value in sorted(wordCount.items(), key = operator.itemgetter(1)):
+        print(key, value)
+
 
 def printListEleents(list):
     print(list)
